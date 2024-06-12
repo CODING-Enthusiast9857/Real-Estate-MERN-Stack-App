b55452a9cea43b8ca3acb0c219ea52de712a4c6c
Madhavi Sonawane <madhavison06@gmail.com>
2024-06-12 20:40:35 +0530

    Seach bar added, HomePage completed

--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -1,10 +1,37 @@
+import SearchBar from '../../components/searchBar/SearchBar';
 import './homePage.scss';
 
 function HomePage() {
   return (
     <div className='homePage'>
-      <div className='textContainer'>text</div>
-      <div className='imgContainer'>img</div>
+      <div className='textContainer'>
+        <div className='wrapper'>
+            <h1 className='title'>
+                Find <span className='real-estate'>Real Estate</span> & Get Your Dream Place
+            </h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, quaerat. Minima dicta eos culpa! Pariatur excepturi illo maiores, nesciunt expedita velit. Corporis sit facere et assumenda est explicabo esse aliquid!
+            </p>
+            <SearchBar/>
+            <div className='boxes'>
+                <div className='box'>
+                    <h1>16+</h1>
+                    <h2>Years of Experience</h2>
+                </div>
+                <div className='box'>
+                    <h1>200</h1>
+                    <h2>Award Gained</h2>
+                </div>
+                <div className='box'>
+                    <h1>1200+</h1>
+                    <h2>Property Ready</h2>
+                </div>
+            </div>
+        </div>
+      </div>
+      <div className='imgContainer'>
+        <img src="/bg.png" alt="" />
+      </div>
     </div>
   )
 }
