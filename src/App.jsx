691a763edfa8f11818ce0efadf4ccf875c213d7d
Madhavi Sonawane <madhavison06@gmail.com>
2024-06-12 19:18:02 +0530

    layout & navbar implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import Navbar from "./components/navbar/Navbar"
+import HomePage from "./routes/homePage/homePage"
 import "./layout.scss"
 
 function App() {
   return (
     <div className="layout">
-      Hello World
+      <div className="navbar">
+        <Navbar/>
+      </div>
+      <div className="content"> 
+        <HomePage/>
+      </div>
+      
     </div>
   )
 }
---
+++ src/routes/homePage/homePage.jsx
@@ -0,0 +1,12 @@
+import './homePage.scss';
+
+function HomePage() {
+  return (
+    <div className='homePage'>
+      <div className='textContainer'>text</div>
+      <div className='imgContainer'>img</div>
+    </div>
+  )
+}
+
+export default HomePage;
